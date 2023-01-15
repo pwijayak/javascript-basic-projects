112d99cf597ae7f0781230cb6854edafd25c639d
pwijayak <59058784+pwijayak@users.noreply.github.com>
2023-01-15 09:22:12 -0500

    Update app.js

---
+++ 02-counter/setup/app.js
@@ -0,0 +1,36 @@
+let count=0
+
+
+const value=document.getElementById('value')
+const btns=document.querySelectorAll('.btn')
+
+
+btns.forEach((btn)=>btn.addEventListener('click',function(e){
+    let styles=e.currentTarget.classList
+
+
+       if(styles.contains('decrease')){
+        count--
+    
+       }
+  else if(styles.contains('increase')){
+    count ++
+  
+  }
+  else{
+     count=0
+   
+  }
+  if (count>0){
+    value.style.color='green'
+}
+if (count<0){
+    value.style.color='red'
+}
+if (count===0){
+    value.style.color='black'
+}
+
+  value.textContent=count
+  
+}))
